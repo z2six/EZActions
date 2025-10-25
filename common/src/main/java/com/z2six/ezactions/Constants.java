823042e687f16730216d19b59e3753f0a921afa1
Z26 <Z26@D26>
2025-10-25 14:26:33 +0200

    1.21.1 setup & javadoc error fix

---
+++ common/src/main/java/com/z2six/ezactions/Constants.java
@@ -0,0 +1,15 @@
+// MainFile: common/src/main/java/org/z2six/ezactions/Constants.java
+package org.z2six.ezactions;
+
+import com.mojang.logging.LogUtils;
+import org.slf4j.Logger;
+
+public final class Constants {
+    public static final String MOD_ID = "ezactions";
+    public static final String MOD_NAME = "EZActions";
+
+    // Global logger for the mod
+    public static final Logger LOG = LogUtils.getLogger();
+
+    private Constants() {}
+}
